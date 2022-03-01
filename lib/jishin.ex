17aaa50525153446b220d2bd15c36fea4d910390
Michael Daines <953037+defndaines@users.noreply.github.com>
2022-02-28 17:35:29 -0800

    `mix new jishin --sup`

---
+++ lib/jishin.ex
@@ -0,0 +1,18 @@
+defmodule Jishin do
+  @moduledoc """
+  Documentation for `Jishin`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jishin.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jishin_test.exs
@@ -0,0 +1,8 @@
+defmodule JishinTest do
+  use ExUnit.Case
+  doctest Jishin
+
+  test "greets the world" do
+    assert Jishin.hello() == :world
+  end
+end
